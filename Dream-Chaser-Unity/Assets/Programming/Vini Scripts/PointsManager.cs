a988934f7a814c8553ea742645ee481f450be0a7
vinivss <vinivss@hotmail.com>
2023-02-09 18:55:37 -0800

    DT, and collision

---
+++ Dream-Chaser-Unity/Assets/Programming/Vini Scripts/PointsManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsManager : MonoBehaviour
+{
+    [Tooltip("Total points player has earned this level")]
+    int pointsTotal;
+
+    public void AddPoints(int pointsToAdd)
+    {
+        pointsTotal += pointsToAdd;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
