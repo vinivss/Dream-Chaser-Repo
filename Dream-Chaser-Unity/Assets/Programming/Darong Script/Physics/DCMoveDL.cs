86460d97e1f2c299a2a7bb0d161208d39028c018
DarongLi <darongli0312@gmail.com>
2023-02-18 01:30:35 -0800

    onSlope() finished

--- Dream-Chaser-Unity/Assets/Programming/Darong Script/Physics/DCMoveDL.cs
+++ Dream-Chaser-Unity/Assets/Programming/Darong Script/Physics/DCMoveDL.cs
@@ -7,39 +7,76 @@ public class DCMoveDL : MonoBehaviour
 {
     //innate components gotten during Awake for functionality
     ControlsManager inManager;
-    Rigidbody rb;
-
-    //priv hidden variables
-    Vector3 moveDir;
 
     //Inspector Vars
     [Header("Movement Attributes")]
     [Tooltip("Minimum Forward Velocity")]
-    [SerializeField]float minForwardSpeed;
+    [SerializeField] float minForwardSpeed;
     [Tooltip("Maximum threshold before slowly rotating to front")]
     [SerializeField] float rotationThreshold;
 
-    [Header("slope check")]
+    [SerializeField] Transform groundCheck;
+
+    public float drag_force;
+
+    [Header("Platform")]
+    public LayerMask GroundPlatform;
+    public LayerMask SlopePlatform;
+    
+    bool slope;
+
+    [Header("Ground check")]
+    public bool grounded = false;
     public float playerHeight;
+
+    [Header("slope check")]
     public float maxSlopeAngle;
     public RaycastHit slopeDetected;
+
+    [Header("Movement")]
+    public float moveSpeed;
+    public Transform orientation;
+    float horizontalInput;
+    float verticalInput;
+    Vector3 moveDir;
+    Rigidbody rb;
+
+
+
 
 
     private void Awake()
     {
         inManager = GetComponent<ControlsManager>();
         rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+    private void Update()
+    {
+        myInput();
+        //grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, GroundPlatform);
+        grounded = Physics.CheckSphere(groundCheck.position, .1f, GroundPlatform);
+        Debug.Log("Ground check: " + grounded);
+
+        SpeedControl();
     }
 
     //physics management
     private void FixedUpdate()
     {
-        onSlope();
+        GetSlopeMoveDirection();
         MoveCharacter();
+    }
+
+    private void myInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
     }
 
     private void MoveCharacter()
     {
+        /*  vini movement
         Debug.DrawRay(transform.position, transform.forward, Color.red);
         Debug.DrawRay(transform.position, -transform.forward, Color.blue);
 
@@ -48,18 +85,36 @@ public class DCMoveDL : MonoBehaviour
         transform.forward = moveDir;
 
         rb.AddForce((moveDir * minForwardSpeed) + Physics.gravity, ForceMode.Acceleration);
-       
+        */
 
+        moveDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        //rb.AddForce(moveDir.normalized * moveSpeed * 10f, ForceMode.Force);
+        rb.AddForce((moveDir * minForwardSpeed) + Physics.gravity, ForceMode.Acceleration);
+
+
+        // on ground
+        if (grounded)
+        {
+            rb.AddForce(moveDir.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+
+        if (onSlope())
+        {
+            Debug.Log("on slope");
+            rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 20f, ForceMode.Force);
+        }
+
+        rb.useGravity = !onSlope();
 
     }
 
     private bool onSlope()
     {
-        //Debug.Log("check on slope");
+        Debug.Log("check on slope");
         if (Physics.Raycast(transform.position, Vector3.down, out slopeDetected, playerHeight * 0.5f + 0.3f))
         {
             float angle = Vector3.Angle(Vector3.up, slopeDetected.normal);
-            //Debug.Log("on slope");
+            Debug.Log("on slope");
             return angle < maxSlopeAngle && angle != 0;
         }
 
@@ -72,5 +127,18 @@ public class DCMoveDL : MonoBehaviour
         return Vector3.ProjectOnPlane(moveDir, slopeDetected.normal).normalized;
     }
 
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+
+    }
 
 }
