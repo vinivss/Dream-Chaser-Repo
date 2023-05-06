5798867937de4cc9a24fcc1bf11b8aeba34929d0
vinivss <vinivss@hotmail.com>
2023-05-06 00:10:59 -0700

    clickndrag

--- Dream-Chaser-Unity/Assets/Programming/Vini Scripts/Dialogue/ActionNodes/ScreenShakeVN.cs
+++ Dream-Chaser-Unity/Assets/Programming/Vini Scripts/Dialogue/ActionNodes/ScreenShakeVN.cs
@@ -7,7 +7,7 @@ public class ScreenShakeVN : DialogueActionNode
 {
     public override void Action()
     {
-        CinemachineVirtualCamera
+        CinemachineVirtualCamera cinema;
         EndNode();
     }
 
